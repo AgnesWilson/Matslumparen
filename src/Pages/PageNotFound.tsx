1326a7fc84f15c7e9a30fb89c4edf5eca2799888
AgnesWilson <agnes.wilson97@hotmail.com>
2026-01-09 18:44:49 +0100

    ✨ Add page not found page

--- src/Pages/PageNotFound.tsx
+++ src/Pages/PageNotFound.tsx
@@ -1,25 +1,29 @@
 import { Box } from '@mui/material';
 import { FeedbackCard } from '../Components/Organisms/FeedbackCard';
 import { useNavigate } from 'react-router';
+import { HeaderLogo } from '../Fixtures/HeaderLogo';
 
 export const PageNotFound = () => {
   const navigate = useNavigate();
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '100vh',
-        paddingInline: 2,
-      }}
-    >
-      <FeedbackCard
-        headline="Sidan du försöker hitta finns tyvärr inte."
-        textOne="Sidan du försökt hitta finns tyvärr inte. Gå tillbaka till den föregående sidan eller försök igen"
-        buttonText="Tillbaka till startsidan"
-        onButtonClick={() => navigate('/')}
-      />
-    </Box>
+    <>
+      <HeaderLogo />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '100vh',
+          paddingInline: 2,
+        }}
+      >
+        <FeedbackCard
+          headline="Sidan du försöker hitta finns tyvärr inte."
+          textOne="Sidan du försökt hitta finns tyvärr inte. Gå tillbaka till den föregående sidan eller försök igen"
+          buttonText="Tillbaka till startsidan"
+          onButtonClick={() => navigate('/')}
+        />
+      </Box>
+    </>
   );
 };
